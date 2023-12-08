24c5d6075ea698ca013211cb18c9b902ab5eef12
Bruno Dipietro <dipietrobr1@gmail.com>
2023-12-08 20:11:57 -0300

    Cognify App

--- CognifyApp/app/src/main/java/com/example/cognify/graficoActivity.kt
+++ CognifyApp/app/src/main/java/com/example/cognify/graficoActivity.kt
@@ -190,6 +190,7 @@ class graficoActivity : AppCompatActivity() {
 
                     }
                     //mostrarGrafico(listaDatosPresionesCorrectas, listaDatosPresiones)
+                    
 
                 }
 
